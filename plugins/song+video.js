ddd2f300af47bb933cf916a1dd6da9e8b864834c
bikebo <165263612+bikebo@users.noreply.github.com>
2024-08-22 20:11:23 -0700

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,25 +1,38 @@
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yt-search')
+
 cmd({
     pattern: "song",
     desc: "download songs",
     category: "download",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!q) return reply("not find it please give me a song name")    
-const search = await yts(q)
-const data = search.videos[0];
-const url = data.url
+async (conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+    try {
+        if (!q) return reply("Please provide a song name.");
 
-let desc = ''
+        const search = await yts(q);
+        if (!search.videos.length) return reply("Sorry, I couldn't find the song.");
 
-}catch(e)}
-console.log(e)
-reply('${e}')
-}
-})    
+        const data = search.videos[0];
+        const url = data.url;
 
-    
+        // Use api-dylux or another downloader here to get the download link
+        const song = await fg.downloader.ytmp3(url);
+        const { title, dl_link } = song;
+
+        let desc = `*Title:* ${title}\n*Duration:* ${data.timestamp}\n*Views:* ${data.views}\n*Link:* ${url}`;
+
+        await conn.sendMessage(from, {
+            audio: { url: dl_link },
+            mimetype: 'audio/mp4',
+            ptt: false
+        }, { quoted: mek });
+
+        reply(`Here is your song:\n\n${desc}`);
+    } catch (e) {
+        console.log(e);
+        reply(`An error occurred: ${e}`);
+    }
+});
