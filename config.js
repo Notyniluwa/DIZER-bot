1a30af9f959d0a74954953c5442291c51bc4a69e
bikebo <165263612+bikebo@users.noreply.github.com>
2024-08-25 05:02:42 -0700

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "12sk1ZwC#ue6Ni-jfVvK8CjoW6v7Xx4W4zAE4kgPYqJeF5O5_EYo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/600fb1c5551ef67e24d76.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "hey i am alive, i am dizer multidevice whatsapp bot",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || 'true',    
 };
