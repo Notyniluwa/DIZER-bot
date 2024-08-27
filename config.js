df0f40cd26b28d32b0acc49b0c279ada8b000848
bikebo <165263612+bikebo@users.noreply.github.com>
2024-08-26 22:29:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-MONGODB:  process.env.MONGODB || ""  
+    SESSION_ID: process.env.SESSION_ID || "o6thzIyK#mk6bQFGC9-w_rYAPgFlLjeXlWTwdEXBG9YtHaQjKglA",
+    MONGODB: process.env.MONGODB || "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority"
+};
