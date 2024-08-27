5fce63223382c58fd66b5c54b55f0e9c03428d7e
bikebo <165263612+bikebo@users.noreply.github.com>
2024-08-27 05:38:14 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
+
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "o6thzIyK#mk6bQFGC9-w_rYAPgFlLjeXlWTwdEXBG9YtHaQjKglA",
-    MONGODB: process.env.MONGODB || "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority"
+SESSION_ID: process.env.SESSION_ID || "o6thzIyK#mk6bQFGC9-w_rYAPgFlLjeXlWTwdEXBG9YtHaQjKglA",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ? 'true' : process.env.AUTO_READ_STATUS,    
+MONGODB: process.env.MONGODB || "mongodb://mongo:YwaWQTJTuUHeFHPSoTZvLunISgkvYvsE@autorack.proxy.rlwy.net:12762"
 };
