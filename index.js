8747c935ad5095981afb3cff360eadd33638c945
Notyniluwa <155547031+Notyniluwa@users.noreply.github.com>
2024-08-27 06:14:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const { sms, downloadMediaMessage } = require('./lib/msg');
 const axios = require('axios');
 const { File } = require('megajs');
 
-const ownerNumber = ['94773673969'];
+const ownerNumber = ['94781029321'];
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
